79e6f51e6d2e593a2a001fbfe15dc35d503c71fd
jack <1286783182@qq.com>
2023-03-27 13:42:42 +0800

    build: add electron

---
+++ electron/app.ts
@@ -0,0 +1,41 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const { app, BrowserWindow, ipcMain } = require('electron');
+const path = require('path');
+const isDev = require('electron-is-dev');
+
+const createWindow = () => {
+	const win = new BrowserWindow({
+		width: 800,
+		height: 600,
+		webPreferences: {
+			preload: path.join(__dirname, 'preload.ts')
+		}
+	});
+	ipcMain.handle('ping', () => 'pong');
+
+	if (isDev) {
+		// If we are in development mode we load content from localhost server - vite
+		// and open the developer tools
+		win.loadURL('http://localhost:5173');
+		win.webContents.openDevTools();
+	} else {
+		// In all other cases, load the index.html file from the dist folder
+		win.loadURL(`file://${path.join(__dirname, '..', 'dist', 'index.html')}`);
+	}
+};
+
+app.whenReady().then(() => {
+	createWindow();
+
+	app.on('activate', () => {
+		if (BrowserWindow.getAllWindows().length === 0) {
+			createWindow();
+		}
+	});
+});
+
+app.on('window-all-closed', () => {
+	if (process.platform !== 'darwin') {
+		app.quit();
+	}
+});
