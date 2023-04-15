8dfa9ad46036b12815ff5377cbd3e0e891dafd41
D:/Program Files/DevTools/Git/jxk <1286783182@qq.com>
2023-04-15 17:22:15 +0800

    feat: add self history Tool

--- src/components/UsefulTable/components/ColumnSetting/ColumnSetting.jsx
+++ src/components/UsefulTable/components/ColumnSetting/ColumnSetting.jsx
@@ -7,7 +7,29 @@ const ITEMS = [
 		id: 'a1'
 	},
 
-	{ item: <DraggableItemSorter />, id: 'b1' },
+	{
+		item: (
+			<DraggableItemSorter
+				showSelfHistoyTool={true}
+				value={[
+					{ item: 'Write a cool JS library', id: 'a' },
+
+					{ item: 'Make it generic enough', id: 'b' },
+
+					{ item: 'Write README', id: 'c' },
+
+					{ item: 'Create some examples', id: 'd' },
+
+					{ item: 'Spam in Twitter and IRC to promote it', id: 'e' },
+
+					{ item: '???', id: 'f' },
+
+					{ item: 'PROFIT', id: 'g' }
+				]}
+			/>
+		),
+		id: 'b1'
+	},
 
 	{ item: 'Write README', id: 'c1' },
 
--- src/components/UsefulTable/components/DraggableItemSorter/DraggableItemSorter.jsx
+++ src/components/UsefulTable/components/DraggableItemSorter/DraggableItemSorter.jsx
@@ -3,171 +3,220 @@ import React, {
 	useCallback,
 	useEffect,
 	useImperativeHandle,
+	useMemo,
 	useState
 } from 'react';
 import PropTypes from 'prop-types';
 import DraggableItem from './DraggableItem';
 import { useDrop } from 'react-dnd';
 const style = {
-	width: 400
+	width: 'auto'
 };
 const ItemTypes = {
 	CARD: 'item'
 };
-const ITEMS = [
-	{ item: 'Write a cool JS library', id: 'a' },
 
-	{ item: 'Make it generic enough', id: 'b' },
+const DraggableItemSorter = forwardRef(
+	({ onChange, value, showSelfHistoyTool, historyToolRenderer }, ref) => {
+		const [items, setItems] = useState(value);
+		const [historyItem, sethistoryItem] = useState([value]);
+		const [currentIndex, setCurrentIndex] = useState(0);
 
-	{ item: 'Write README', id: 'c' },
+		const allowGoAhead = useCallback(() => {
+			return currentIndex < historyItem.length - 1;
+		}, [currentIndex, historyItem.length]);
 
-	{ item: 'Create some examples', id: 'd' },
+		const revert = useCallback(() => {
+			setItems(historyItem[0]);
+			setCurrentIndex(0);
+		}, [historyItem]);
 
-	{ item: 'Spam in Twitter and IRC to promote it', id: 'e' },
+		const redo = useCallback(() => {
+			setItems(historyItem[historyItem.length - 1]);
+			setCurrentIndex(historyItem.length - 1);
+		}, [historyItem]);
 
-	{ item: '???', id: 'f' },
-
-	{ item: 'PROFIT', id: 'g' }
-];
-const DraggableItemSorter = forwardRef(({ onChange, value }, ref) => {
-	const [items, setItems] = useState(value ?? ITEMS);
-	const [historyItem, sethistoryItem] = useState([value ?? ITEMS]);
-	const [currentIndex, setCurrentIndex] = useState(0);
-
-	useImperativeHandle(
-		ref,
-		() => {
-			return {
-				allowGoAhead() {
-					return currentIndex < historyItem.length - 1;
-				},
-				revert() {
-					setItems(historyItem[0]);
-					setCurrentIndex(0);
-				},
-				redo() {
-					setItems(historyItem[historyItem.length - 1]);
-					setCurrentIndex(historyItem.length - 1);
-				},
-				goAhead() {
-					setCurrentIndex((index) => {
-						const newIndex = index + 1;
-						const newItems = historyItem[newIndex];
-						if (newItems) {
-							setItems(newItems);
-							return newIndex;
-						} else {
-							return index;
-						}
-					});
-					const newItems = historyItem[currentIndex + 1];
-					if (newItems) setItems(newItems);
-				},
-				allowGoBack() {
-					return currentIndex > 0;
-				},
-				goBack() {
-					setCurrentIndex((index) => {
-						const newIndex = index - 1;
-						const newItems = historyItem[newIndex];
-						if (newItems) {
-							setItems(newItems);
-							return newIndex;
-						} else {
-							return index;
-						}
-					});
+		const goAhead = useCallback(() => {
+			setCurrentIndex((index) => {
+				const newIndex = index + 1;
+				const newItems = historyItem[newIndex];
+				if (newItems) {
+					setItems(newItems);
+					return newIndex;
+				} else {
+					return index;
 				}
-			};
-		},
-		[currentIndex, historyItem]
-	);
-
-	useEffect(() => {
-		onChange?.(historyItem[currentIndex]);
-	}, [historyItem, currentIndex, onChange]);
-
-	const findItem = useCallback(
-		(id) => {
-			const item = items.filter((c) => `${c.id}` === id)[0];
-			return {
-				item,
-				index: items.indexOf(item)
-			};
-		},
-		[items]
-	);
-	const moveItem = useCallback(
-		(id, atIndex) => {
-			const { item, index } = findItem(id);
-			const newItems = [...items];
-			console.log(newItems, 'newItems');
-			if (item) {
-				newItems.splice(index, 1);
-				newItems.splice(atIndex, 0, item);
-			}
-			setItems(newItems);
-		},
-		[findItem, items]
-	);
-
-	const removeItem = useCallback(
-		(id) => {
-			setCurrentIndex((index) => index + 1);
-			setItems((preItems) => {
-				const newItems = [...preItems];
-				const { index } = findItem(id);
-				newItems.splice(index, 1);
-
-				const currnthistoryItem = historyItem.slice(0, currentIndex + 1);
-				currnthistoryItem.push(newItems);
-				sethistoryItem(currnthistoryItem);
-				return newItems;
 			});
-		},
-		[findItem, historyItem, currentIndex]
-	);
-	const [, drop] = useDrop(() => ({
-		accept: ItemTypes.CARD
-	}));
+			const newItems = historyItem[currentIndex + 1];
+			if (newItems) setItems(newItems);
+		}, [currentIndex, historyItem]);
 
-	const handleItemDragEnd = useCallback(() => {
-		setCurrentIndex((index) => {
-			const newIndex = index + 1;
-			sethistoryItem((preHistoryItem) => {
-				const currnthistoryItem = preHistoryItem.slice(0, newIndex);
-				currnthistoryItem.push(items);
-				return currnthistoryItem;
+		const allowGoBack = useCallback(() => {
+			return currentIndex > 0;
+		}, [currentIndex]);
+
+		const goBack = useCallback(() => {
+			setCurrentIndex((index) => {
+				const newIndex = index - 1;
+				const newItems = historyItem[newIndex];
+				if (newItems) {
+					setItems(newItems);
+					return newIndex;
+				} else {
+					return index;
+				}
 			});
-			return newIndex;
-		});
-	}, [items]);
-	return (
-		<div ref={drop} style={style}>
-			{items
-				.filter((item) => {
-					if (!item) return false;
-					return !!item;
-				})
-				.map((item) => (
-					<DraggableItem
-						key={item.id}
-						id={`${item?.id}`}
-						item={item.item}
-						moveItem={moveItem}
-						findItem={findItem}
-						removeItem={removeItem}
-						// deleteIcon={'x'}
-						onDragEnd={handleItemDragEnd}
-					/>
-				))}
-		</div>
-	);
-});
+		}, [historyItem]);
+
+		useImperativeHandle(
+			ref,
+			() => {
+				return {
+					revert,
+					redo,
+					allowGoAhead,
+					goAhead,
+					allowGoBack,
+					goBack
+				};
+			},
+			[allowGoAhead, allowGoBack, goAhead, goBack, redo, revert]
+		);
+
+		useEffect(() => {
+			onChange?.(historyItem[currentIndex]);
+		}, [historyItem, currentIndex, onChange]);
+
+		const findItem = useCallback(
+			(id) => {
+				const item = items.filter((c) => `${c.id}` === id)[0];
+				return {
+					item,
+					index: items.indexOf(item)
+				};
+			},
+			[items]
+		);
+		const moveItem = useCallback(
+			(id, atIndex) => {
+				const { item, index } = findItem(id);
+				const newItems = [...items];
+				if (item) {
+					newItems.splice(index, 1);
+					newItems.splice(atIndex, 0, item);
+				}
+				setItems(newItems);
+			},
+			[findItem, items]
+		);
+
+		const removeItem = useCallback(
+			(id) => {
+				setCurrentIndex((index) => index + 1);
+				setItems((preItems) => {
+					const newItems = [...preItems];
+					const { index } = findItem(id);
+					newItems.splice(index, 1);
+
+					const currnthistoryItem = historyItem.slice(0, currentIndex + 1);
+					currnthistoryItem.push(newItems);
+					sethistoryItem(currnthistoryItem);
+					return newItems;
+				});
+			},
+			[findItem, historyItem, currentIndex]
+		);
+		const [, drop] = useDrop(() => ({
+			accept: ItemTypes.CARD
+		}));
+
+		const handleItemDragEnd = useCallback(() => {
+			setCurrentIndex((index) => {
+				const newIndex = index + 1;
+				sethistoryItem((preHistoryItem) => {
+					const currnthistoryItem = preHistoryItem.slice(0, newIndex);
+					currnthistoryItem.push(items);
+					return currnthistoryItem;
+				});
+				return newIndex;
+			});
+		}, [items]);
+
+		const selfHistoyTool = useMemo(() => {
+			return historyToolRenderer({
+				allowGoAhead,
+				allowGoBack,
+				revert,
+				redo,
+				goAhead,
+				goBack
+			});
+		}, [
+			allowGoAhead,
+			allowGoBack,
+			goAhead,
+			goBack,
+			historyToolRenderer,
+			redo,
+			revert
+		]);
+		return (
+			<div style={{ display: 'flex', flexDirection: 'column', flex: 1 }}>
+				{showSelfHistoyTool && selfHistoyTool}
+				<div ref={drop} style={style}>
+					{items &&
+						items
+							.filter((item) => {
+								if (!item) return false;
+								return !!item;
+							})
+							.map((item) => (
+								<DraggableItem
+									key={item.id}
+									id={`${item?.id}`}
+									item={item.item}
+									moveItem={moveItem}
+									findItem={findItem}
+									removeItem={removeItem}
+									// deleteIcon={'x'}
+									onDragEnd={handleItemDragEnd}
+								/>
+							))}
+				</div>
+			</div>
+		);
+	}
+);
+DraggableItemSorter.defaultProps = {
+	showSelfHistoyTool: false,
+	historyToolRenderer: ({
+		allowGoAhead,
+		allowGoBack,
+		revert,
+		redo,
+		goAhead,
+		goBack
+	}) => {
+		return (
+			<div style={{ display: 'flex', gap: 8 }}>
+				<button disabled={!allowGoAhead()} onClick={goAhead}>
+					go ahead
+				</button>
+				<button disabled={!allowGoBack()} onClick={goBack}>
+					go back
+				</button>
+				<button onClick={revert}>revert</button>
+				<button onClick={redo}>redo</button>
+			</div>
+		);
+	}
+};
 
 DraggableItemSorter.propTypes = {
 	onChange: PropTypes.func,
-	value: PropTypes.array
+	value: PropTypes.array,
+	showSelfHistoyTool: PropTypes.bool,
+	historyToolRenderer: PropTypes.func
 };
 DraggableItemSorter.displayName = 'DraggableItemSorter';
 
